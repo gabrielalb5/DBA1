db2d446c2b59b55eea3b4cee42ce2cd97a4b7a87
Gabriel Albino <gabriel.albino@aluno.ifsp.edu.br>
2025-05-26 18:16:47 -0300

    atividade de select e junção 2 finalizada

--- AtividadeSelectJuncao2_GabrielAlbino.sql
+++ AtividadeSelectJuncao2_GabrielAlbino.sql
@@ -1,3 +1,5 @@
+use world;
+
 -- 1. Quantas linguagens diferentes existem?
 select count(distinct language) 'Qtd. Línguas existentes'
 from countrylanguage;
@@ -46,9 +48,90 @@ select count(distinct cl.language) 'Qtd. Línguas não oficiais'
 from countrylanguage cl
 where cl.isofficial = 'F';
 
-
 -- 9. Quais as 5 cidades mais populosas do mundo?
 select name 'Cidade', population 'População'
 from city
 order by population desc
 limit 5;
+
+-- 10. Em quais países estão as 5 cidades mais populosas do mundo?
+select ct.name 'Cidade', ct.population 'População', c.name 'País'
+from city ct, country c
+where ct.countrycode = c.code
+order by ct.population desc
+limit 5;
+
+-- 11. Qual a cidade do mundo tem a menor população?
+select ct.name 'Cidade', ct.population 'População'
+from city ct
+order by ct.population
+limit 1;
+
+-- 12. Em qual país está a cidade que tem a menor população do mundo?
+select c.name 'País'
+from country c, city ct
+where ct.countrycode = c.code
+order by ct.population
+limit 1;
+
+-- 13. Qual a cidade da Europa tem a menor população?
+select ct.name 'Cidade', ct.population 'População', c.name 'País'
+from city ct, country c
+where ct.countrycode = c.code
+and c.continent = 'Europe'
+order by ct.population
+limit 1;
+
+-- 14. Exiba os continentes e suas populações em ordem decrescente.
+select continent, sum(population) 'População'
+from country
+group by continent
+order by sum(population);
+
+-- 15. Qual a expectativa de vida média no mundo?
+select avg(lifeexpectancy)
+from country;
+
+-- 16. Quais países têm expectativa de vida maior do que a média mundial?
+select name 'País', lifeexpectancy 'Expectativa de vida'
+from country
+where lifeexpectancy > (select avg(lifeexpectancy) from country)
+order by lifeexpectancy desc;
+
+-- 17. Qual o país que tem a menor expectativa de vida no mundo?
+select name 'País', lifeexpectancy 'Expectativa de vida'
+from country
+where lifeexpectancy is not null
+order by lifeexpectancy asc
+limit 1;
+
+-- 18. Quais os 5 maiores países do mundo em relação ao seu tamanho de área?
+select name 'País', surfacearea 'Tamanho do território'
+from country
+order by surfacearea desc
+limit 5;
+
+-- 19. Quais os 3 maiores países da Europa em relação ao seu tamanho de área?
+select name 'País', surfacearea 'Tamanho do território'
+from country
+where continent = 'Europe'
+order by surfacearea desc
+limit 3;
+
+-- 20. Exiba os nomes dos países (e de suas capitais) da América do Sul.
+select c.name 'País', ct.name 'Capital'
+from country c, city ct
+where c.capital = ct.id
+and continent = 'South America'
+order by c.name;
+
+-- 21. Qual o nome do país que tem como capital Toskent?
+select c.name 'País', ct.name 'Capital'
+from country c, city ct
+where c.capital = ct.id
+and ct.name = 'Toskent';
+
+-- 22. Existe algum país no mundo que não tenha capital?
+select name 'País'
+from country
+where capital is null;
